a141e03459bbb1d80bf8e028a18c63915cc84437
April Skrine <april.skrine@gmail.com>
2022-01-18 12:58:30 -0600

    Pushed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.getElementById("create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    dealWithToDo(e.target["new-task-description"].value)
+  })
+})
+
+function dealWithToDo(todo) {
+  let p = document.createElement('p')
+  let bttn = document.createElement('button')
+  bttn.textContent = "x"
+  bttn.addEventListener('click', deleteToDo)
+  p.textContent = `${todo} `
+  p.appendChild(bttn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove()
+}
